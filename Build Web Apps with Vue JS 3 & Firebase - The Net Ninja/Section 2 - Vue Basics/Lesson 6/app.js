ea9686b53faeeadca2ff4cf330ffe59086bb7db9
Mark McClenaghan <mark.mcclenaghan@farmvetsystems.com>
2023-12-28 18:58:08 +0000

    v-if and mouse events

--- Build Web Apps with Vue JS 3 & Firebase - The Net Ninja/Section 2 - Vue Basics/Lesson 6/app.js
+++ Build Web Apps with Vue JS 3 & Firebase - The Net Ninja/Section 2 - Vue Basics/Lesson 6/app.js
@@ -3,14 +3,28 @@ const app = Vue.createApp({
     //template: '<h2>I am a template</h2>'
     data() {
         return {
+            showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
-        changeTitle(title) {
+        /*changeTitle(title) {
             this.title = title
+        }*/
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data){
+            console.log(e, e.type);
+            if(data) console.log(data);
+        },
+        handleMouseMove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
