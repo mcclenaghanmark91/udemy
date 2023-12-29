57597f7a77da40550a80fd14749b4a6baebaa0af
Mark McClenaghan <mark.mcclenaghan@farmvetsystems.com>
2023-12-29 00:56:01 +0000

    Up to Challenge on reusing components

--- Build Web Apps with Vue JS 3 and Firebase - The Net Ninja/Section 3 - The Vue CLI/modal-project/src/App.vue
+++ Build Web Apps with Vue JS 3 and Firebase - The Net Ninja/Section 3 - The Vue CLI/modal-project/src/App.vue
@@ -1,22 +1,37 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick">click me</button>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal theme="" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+
+    </Modal>/>
+  </div>
+  <button @click.alt="toggleModal">open modal (alt)</button>
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
+
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return{
-      title: 'My First Vue App! :)'
+      title: 'My First Vue App!',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your Ninja swag for half price!',
+      showModal: false
     }
   },
   methods: {
-    handleClick() {
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
+    toggleModal() {
+      this.showModal = !this.showModal
     }
   }
 }
