382b07408e478e9c01e2489aa22d2703f8296e22
Mark McClenaghan <mark.mcclenaghan@farmvetsystems.com>
2024-01-04 22:56:14 +0000

    Dynamic links using props

--- Build Web Apps with Vue JS 3 and Firebase - The Net Ninja/Section 6 - Vue Router Basics/ninja-jobs/src/router/index.js
+++ Build Web Apps with Vue JS 3 and Firebase - The Net Ninja/Section 6 - Vue Router Basics/ninja-jobs/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/HomeView.vue'
 import About from '../views/AboutView.vue'
+import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
 
 const routes = [
   {
@@ -12,6 +14,17 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',
+    name: 'jobDetails',
+    component: JobDetails,
+    props: true
   }
 ]
 
